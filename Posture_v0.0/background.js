f6d7332c5954bf40129afb3e4023b45eebda749a
Ethan Weygang <37845554+EthanWeygang@users.noreply.github.com>
2024-06-08 23:47:19 +0100

    Fixed spam bug and Sound toggle added

--- Posture_v0.0/background.js
+++ Posture_v0.0/background.js
@@ -1,45 +1,77 @@
-chrome.runtime.onMessage.addListener(function(request){
+const quips = ["Check your posture!", "funny response B", "funny response C"]
 
-    const quips = ["Check your posture!", "funny response B", "funny response C"]
+chrome.runtime.onMessage.addListener(function(request){ // Makes listener which executes code every time button is pressed
 
     if (request.time){ //If message is to set alarm
-        
-        chrome.alarms.clearAll() //Clear previous alarm - CHROME CANT KEEP UP IF SPAMMED. CANT FIX?
-        console.log("Alarm cleared")// Maybe make a check to see if theres multiple alarms when executing notificaiton, if there is then clear.
+
         chrome.alarms.create("Posture Alarm", { //Create alarm
             periodInMinutes: 1,
             delayInMinutes: parseInt(request.time)
         })
-        console.log("Alarm created");
+        console.log("Alarm created for " + request.time + " minute(s)");
     }
     else if (request.clear){ //If message is to clear alarm
-        chrome.alarms.clear("Posture Alarm"); //Clear alarm
-        console.log("Alarm cleared");
+        
+        chrome.alarms.clear("Posture Alarm", (wasCleared) => { //Clear alarm and recieve result
+            if (wasCleared){
+                console.log("Alarm cleared");
+            }
+            else { 
+                console.log("Attempted clear but no alarms found")
+            }
+        }); 
+        
     };
+});
 
-    
-    chrome.alarms.onAlarm.addListener(() => { //When alarm is triggered
+chrome.alarms.onAlarm.addListener(() => { //Add listener which executes when alarm finishes
+    chrome.storage.sync.get("soundsOn", function(result){
+        if (result.soundsOn == true){
+            var notif = {
+                type: "basic",
+                title: "Posture Checker",
+                iconUrl: "content/48x48_trans.png",
+                message: quips[0],
+                silent: true,
+                requireInteraction: true}
+                console.log("WORKING")
+        }
+        else {
+            var notif = {
+                type: "basic",
+                title: "Posture Checker",
+                iconUrl: "content/48x48_trans.png",
+                message: quips[0],
+                silent: false,
+                requireInteraction: true}
+                
+        }
 
-        var notif = {
-            type: "basic",
-            title: "Posture Checker",
-            iconUrl: "content/48x48_trans.png",
-            message: quips[0],
-            silent: true
-        };
         chrome.notifications.create("notifId", notif); //Create notification
+    console.log("Notification sent")
 
-        chrome.windows.create({ //Create window
+    /*chrome.windows.create({ //Create window (Manifest v3 doesnt let you play sounds without an open window :') )
 
-            url: chrome.runtime.getURL("notification.html"),
-            type: "popup",
-            focused: false,
-            state: "minimized"
-            } , function(window){
+        url: chrome.runtime.getURL("notification.html"),
+        height: 1,
+        width: 1,
+        left: -1000,
+        top: -1000,
+        type: "popup",
+        focused: false,
+        state: "normal", 
+        
+        } , function(window){
 
-            setTimeout(function(){ //Close window after 1 second
-                chrome.windows.remove(window.id);
-            }, 1500);
-        });
-    });
+        setTimeout(function(){ //Close window after sound plays 
+            chrome.windows.remove(window.id);
+        }, 700);
+    });*/
+    })
+});
+
+chrome.runtime.onStartup.addListener(() => { //Resent popup (Useful if chrome was quit whilst timer was active)
+    
+    chrome.storage.sync.set({"buttonstate": "inactive"})
+    console.log("Chrome Starting, popup reset to default")
 });
