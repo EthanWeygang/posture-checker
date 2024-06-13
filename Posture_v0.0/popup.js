25c9a83eb7069646baf6a72dddd2befe93ca1089
Ethan Weygang <37845554+EthanWeygang@users.noreply.github.com>
2024-06-13 02:33:04 +0100

    Mute finished & code/UI cleanup

--- Posture_v0.0/popup.js
+++ Posture_v0.0/popup.js
@@ -1,48 +1,56 @@
 $(document).ready(function() {
-    chrome.storage.sync.get(['sliderval', 'buttonstate'], function(result) {
+    // Prepares popup when opened
+    chrome.storage.sync.get(['sliderval', 'started', 'soundsOn'], function(result) {
         $("#slider").val(result.sliderval);
         $("#sliderval").text(result.sliderval);
 
-        if (result.buttonstate === "active") { // Checks in storage if the button was active when last closed
-            $("#buttoninactive").text("Stop")
-            $("#buttoninactive").attr("id", "buttonactive")
-            $("#slider").attr("disabled", "true")
+        if (result.started === true) { // Checks in storage if the button was active when last closed
+            $("#startbutton").text("Stop");
+            $("#slider").attr("disabled", "true");
+        }
+
+        if ((result.soundsOn) === false){
+            $("#sounds").text("Muted");
         }
     });
 
-   
-    $(document).on("click", "#buttoninactive", function(){
-        chrome.runtime.sendMessage({time: $("#slider").val()});
-        console.log("Alarm message sent");
+    // Toggles timer
+   $(document).on("click", "#startbutton", function(){
+        chrome.storage.sync.get("started", function(result){
+            if (result.started === false){
 
-        $("#buttoninactive").text("Stop");
-        $("#buttoninactive").attr("id", "buttonactive"); // Changes button to active
-        $("#slider").attr("disabled", "true");
-        chrome.storage.sync.set({"buttonstate": "active"}); // Changes storage variable to active
-    });
+                chrome.runtime.sendMessage({time: $("#slider").val()});
+                $("#startbutton").text("Stop");
+                $("#slider").attr("disabled", "true");
+                
+                chrome.storage.sync.set({"started": true});
+            }
+            else{
+                chrome.runtime.sendMessage({clear: true});
+                $("#startbutton").text("Start");
+                $("#slider").removeAttr("disabled");
+                
+                chrome.storage.sync.set({"started": false});
+            }
+        });
+   });
 
-    $(document).on("click", "#buttonactive", function(){
-        chrome.runtime.sendMessage({clear: true});
-        console.log("Alarm cleared");
-
-        $("#buttonactive").text("Start");
-        $("#buttonactive").attr("id", "buttoninactive"); // Changes button to inactive
-        $("#slider").removeAttr("disabled");
-        chrome.storage.sync.set({"buttonstate": "inactive"}); // Changes storage variable to inactive
-    });
-
+    // Toggles sounds
     $(document).on("click", "#sounds", function(){
         chrome.storage.sync.get("soundsOn", function(result) {
             
-            if (result.soundsOn){
+            if (result.soundsOn === true){
+                $("#sounds").text("Muted")
                 chrome.storage.sync.set({"soundsOn": false})
             } 
             else{
+                $("#sounds").text("Unmuted")
                 chrome.storage.sync.set({"soundsOn": true})
             }
         });
-    })
+    });
     
+    // Updates slider value display
     $("#slider").on("input", function(){
         $("#sliderval").text($("#slider").val().toString());
         chrome.storage.sync.set({ "sliderval": $("#slider").val() });
